ec41fc7d0e95b001a91f4911979955df4db9f2f3
Ihor Homenko <igorhomenko97@gmail.com>
2021-12-04 22:01:36 +0200

    first example

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// preventDefault()
+
+// console.log(galleryItems);
+
+const galleryCards = createGalleryCards (galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML('afterbegin', galleryCards);
+
+gallery.addEventListener('click', onGetUrlLaggeImgClick);
+
+
+function createGalleryCards (gallery) {
+    return gallery.map(({preview, original, description}) => `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`).join('')
+}
+
+function onGetUrlLaggeImgClick (evt) {
+  evt.preventDefault();
+
+  const galleryElLink = evt.target;
+  if(!galleryElLink) {
+    return
+  }
+
+  const galleryLargeImgUrl = evt.target.parentNode.href;
+
+  modalLigthBasic(galleryLargeImgUrl);
+}
+
+function modalLigthBasic (url) {
+  const instance = basicLightbox.create(`
+    <img src="${url}">
+`)
+
+instance.show()
+// instance.close(() => console.log('lightbox not visible anymore'))
+}
+
+
